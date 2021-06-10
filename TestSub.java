dd1c73cccf847f5c4be4f4d464276753e1f3fa5c
bipin thapa <bipzzzlogin@gmail.com>
2021-06-10 17:48:42 +0545

    first commit

---
+++ TestSub.java
@@ -0,0 +1,11 @@
+public class TestSub
+{  
+public static void main(String[]args)  
+{ 
+  int a,b,c;  
+  a=Integer.parseInt(args[0]);
+  b=Integer.parseInt(args[1]);
+  c=a+b;
+System.out.println("sum="+c); 
+ } 
+} 
